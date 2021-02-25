2449b79c697906685b667d7ca5f27b6b22fb4caf
unknown <ckosek@gmx.com>
2021-02-25 13:16:00 -0500

    Options Menu added

--- MainMenu.py
+++ MainMenu.py
@@ -4,36 +4,54 @@ import sys
 import pygame as pg
 import pygame_menu as pm
 
-pg.init()
-surface = pg.display.set_mode((0,0),pg.FULLSCREEN)
+def main_menu():
+	pg.init()
+	surface = pg.display.set_mode((0,0),pg.FULLSCREEN)
 
-def set_difficulty(value, difficulty):
-	# Do the job here !
-	pass
+	def set_difficulty(value, difficulty):
+		# Do the job here !
+		pass
 
-def start_the_game():
-	# Do the job here !
-	pass
+	def start_the_game():
+		# Do the job here !
+		pass
+
+
+	eight_bit_font = pm.font.FONT_8BIT
+	title_theme = pm.widgets.MENUBAR_STYLE_NONE
+	mytheme = pm.themes.Theme(background_color=(0,0,0), widget_font=eight_bit_font, title_bar_style=title_theme, 
+							menubar_close_button=True, title_font=eight_bit_font, title_font_color=(255,50,50))
+
+	menu = pm.Menu(700, 700, 'The Ship Predicament', theme=mytheme)
+	#menu = pm.Menu(500, 500, 'The Ship Predicament', theme=pm.themes.THEME_DARK)
+
+	#menu.add_text_input('Name :', default='John Doe')
+	menu.add_selector('Difficulty :', [('Easy', 1), ('Medium', 2), ('Hard', 3)], onchange=set_difficulty, font_color=(255,50,50))
+	menu.add_button('Play', start_the_game, font_color=(255,50,50))
+	menu.add_button('Options', options_menu, font_color=(255,50,50))
+	menu.add_button('Quit', pm.events.EXIT, font_color=(255,50,50))
+
+	menu.mainloop(surface)
+
 
 def options_menu():
-	#Do the job here !
-	pass
+	pg.init()
+	surface = pg.display.set_mode((0,0),pg.FULLSCREEN)
 
-eight_bit_font = pm.font.FONT_8BIT
-title_theme = pm.widgets.MENUBAR_STYLE_NONE
-mytheme = pm.themes.Theme(background_color=(0,0,0), widget_font=eight_bit_font, title_bar_style=title_theme, 
-						  menubar_close_button=True, title_font=eight_bit_font, title_font_color=(255,50,50))
+	eight_bit_font = pm.font.FONT_8BIT
+	title_theme = pm.widgets.MENUBAR_STYLE_NONE
+	mytheme = pm.themes.Theme(background_color=(0,0,0), widget_font=eight_bit_font, title_bar_style=title_theme, 
+							menubar_close_button=True, title_font=eight_bit_font, title_font_color=(255,50,50))
 
-menu = pm.Menu(700, 700, 'The Ship Predicament', theme=mytheme)
-#menu = pm.Menu(500, 500, 'The Ship Predicament', theme=pm.themes.THEME_DARK)
+	menu = pm.Menu(700, 700, 'Options', theme=mytheme)
+	#menu = pm.Menu(500, 500, 'The Ship Predicament', theme=pm.themes.THEME_DARK)
 
-#menu.add_text_input('Name :', default='John Doe')
-menu.add_selector('Difficulty :', [('Easy', 1), ('Medium', 2), ('Hard', 3)], onchange=set_difficulty, font_color=(255,50,50))
-menu.add_button('Play', start_the_game, font_color=(255,50,50))
-menu.add_button('Options', options_menu, font_color=(255,50,50))
-menu.add_button('Quit', pm.events.EXIT, font_color=(255,50,50))
+	#menu.add_text_input('Name :', default='John Doe')
+	menu.add_button('button', main_menu, font_color=(255,50,50))
+	menu.add_button('Back to Main Menu', main_menu, font_color=(255,50,50))
 
-menu.mainloop(surface)
+	menu.mainloop(surface)
 
-#if __name__ == '__main__':
-#	print('hello')
+if __name__ == '__main__':
+	main_menu()
+
