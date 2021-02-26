ee6f1fe7eaccebb34e32b1f1f848b01bedad1651
Robert A McDonald <robalmcdonald@gmail.com>
2021-02-25 19:47:36 -0500

    Added color list, added volume selection and method for options menu.

--- MainMenu.py
+++ MainMenu.py
@@ -4,64 +4,90 @@ import sys
 import pygame as pg
 import pygame_menu as pm
 
-color = (255,50,50)
+#Color List for quick reference
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+LIME = (0,255,0)
+BLUE = (0,0,255)
+YELLOW = (255,255,0)
+CYAN = (0,255,255)
+MAGENTA = (255,0,255)
+SILVER = (192,192,192)
+GRAY = (128,128,128)
+MAROON = (128,0,0)
+OLIVE = (128,128,0)
+GREEN = (0,128,0)
+PURPLE = (128,0,128)
+TEAL = (0,128,128)
+NAVY = (0,0,128)
+
+#Color set for Text
+color = RED
 color_default = 0
 
-back_color = (0,0,0)
+#Color set for Background
+back_color = BLACK
 back_color_default = 0
 
+#Method: Sets color for text based on input
 def set_color(name, value):
 	global color
 	global color_default
 	# Changes color to options value selection
 	if value == 1:
-		color = (255,50,50) #RED
+		color = RED #RED
 		color_default = value-1
 		options_menu()
 	if value == 2:
-		color = (0,0,255) #BLUE
+		color = BLUE #BLUE
 		color_default = value-1
 		options_menu()
 	if value == 3:
-		color = (0,255,0) #GREEN
+		color = GREEN #GREEN
 		color_default = value-1
 		options_menu()
 	else:
-		color = (255,50,50) #RED BACKUP
+		color = RED #RED BACKUP
 		options_menu()
 
+#Method: sets color of background based on input
 def set_background(name, value):
 	global back_color
 	global back_color_default
 	# Changes color to options value selection for menu background
 	if value == 1:
-		back_color = (0,0,0) #BLACK
+		back_color = BLACK #BLACK
 		back_color_default = value-1
 		options_menu()
 	if value == 2:
-		back_color = (255,255,255) #WHITE
+		back_color = WHITE #WHITE
 		back_color_default = value-1
 		options_menu()
 	if value == 3:
-		back_color = (128,128,128) #GRAY
+		back_color = GRAY #GRAY
 		back_color_default = value-1
 		options_menu()
 	else:
-		back_color = (0,0,0) #BLACK BACKUP
+		back_color = BLACK #BLACK BACKUP
 		options_menu()
 
 def set_theme():
 	eight_bit_font = pm.font.FONT_8BIT
 	title_theme = pm.widgets.MENUBAR_STYLE_NONE
 	font_size = 37
-	select_box_color = (255,255,255)
-	if back_color == (255,255,255):
-		select_box_color = (0,0,0)
+	select_box_color = WHITE
+	if back_color == WHITE:
+		select_box_color = BLACK
 		
 	current_theme = pm.themes.Theme(background_color=back_color, widget_font=eight_bit_font, title_bar_style=title_theme, 
 							menubar_close_button=True, title_font=eight_bit_font, title_font_color=color, 
 							title_font_size=font_size, selection_color=select_box_color)
 	return current_theme
+
+def set_volume(name, value):
+	#Set up volume level
+	pass
 
 def set_difficulty(value, difficulty):
 	# Do the job here !
@@ -98,11 +124,20 @@ def options_menu():
 
 	mytheme = set_theme()
 
+	volume_int_list = []
+	volume_str_list = []
+	volume_int_list = [i for i in range(0,101)]
+	for a in volume_int_list:
+		a = str(a)
+		volume_str_list.append(a)
+	volume_items = list(zip(volume_str_list, volume_int_list))
+	
 	menu = pm.Menu(700, 700, 'Options', theme=mytheme)
 
 	menu.add_label('Press Enter To')
 	menu.add_label('Apply Selected Item')
 	menu.add_vertical_margin(30)
+	menu.add_selector('Volume ', volume_items, onchange=set_volume, font_color=color)
 	menu.add_selector('Text Color ', [('Red', 1), ('Blue', 2), ('Green', 3)], default=color_default, onreturn=set_color, font_color=color)
 	menu.add_selector('Background ', [('Black', 1), ('White', 2), ('Gray', 3)], default=back_color_default, onreturn=set_background, font_color=color)
 	menu.add_vertical_margin(50)
