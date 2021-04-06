48c3dd3385d0d980c28e39ec1848aff039cc172e
ajturn03 <52827342+ajturn03@users.noreply.github.com>
2021-04-06 15:28:05 -0400

    Add files via upload
    Added the explosion(hit) and splash(miss) effects.

---
+++ Game.py
@@ -0,0 +1,488 @@
+import pygame as pg
+import sys, random
+from pygame.locals import *
+
+#Initiating pygame
+pg.init()
+
+#Set surface as Fullscreen Display
+surface = pg.display.set_mode((0,0),pg.FULLSCREEN)
+monitor_info = pg.display.Info()
+w = monitor_info.current_w
+h = monitor_info.current_h
+		
+#Clock and Framerate
+clock = pg.time.Clock()
+FPS = 60
+
+#How fast the explosion will loop through all images
+ExplosionSpeed = 10
+#How fast the splash will loop through all images
+SplashSpeed = 10
+#Declares the images used for the explosion animation
+Explosion_Images = [
+	pg.image.load("img/explosion0.png"), pg.image.load("img/explosion1.png"), pg.image.load("img/explosion2.png"),
+	pg.image.load("img/explosion3.png"), pg.image.load("img/explosion4.png"), pg.image.load("img/explosion5.png"),
+	pg.image.load("img/explosion6.png"), pg.image.load("img/explosion7.png"), pg.image.load("img/explosion8.png"),
+	pg.image.load("img/explosion9.png"), pg.image.load("img/explosion10.png"), pg.image.load("img/explosion11.png"),
+	pg.image.load("img/explosion12.png"), pg.image.load("img/explosion13.png"), pg.image.load("img/explosion14.png"),
+	pg.image.load("img/explosion15.png"),  pg.image.load("img/explosion16.png"),  pg.image.load("img/explosion17.png"),
+	pg.image.load("img/explosion18.png"), pg.image.load("img/explosion19.png"), pg.image.load("img/explosion20.png"),
+	pg.image.load("img/explosion21.png"), pg.image.load("img/explosion22.png"), pg.image.load("img/explosion23.png"),
+	pg.image.load("img/explosion24.png")
+]
+#Declares the images used for the splash animation
+Splash_Images = [
+	pg.image.load("img/splash0.png"), pg.image.load("img/splash1.png"), pg.image.load("img/splash2.png"),
+	pg.image.load("img/splash3.png"), pg.image.load("img/splash4.png"), pg.image.load("img/splash5.png"),
+	pg.image.load("img/splash0.png"), pg.image.load("img/splash0.png")
+]
+
+#Color List for quick reference
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+LIME = (0,255,0)
+BLUE = (0,0,255)
+YELLOW = (255,255,0)
+CYAN = (0,255,255)
+MAGENTA = (255,0,255)
+SILVER = (192,192,192)
+GRAY = (128,128,128)
+MAROON = (128,0,0)
+OLIVE = (128,128,0)
+GREEN = (0,128,0)
+PURPLE = (128,0,128)
+TEAL = (0,128,128)
+NAVY = (0,0,128)
+
+#Global colors relating to Menu themes
+text_color = RED
+back_color = BLACK
+
+#-----------Class Definitions-----------#
+class Button:
+	def __init__(self, xPos, yPos, width, height, color, surface):
+		self.xPos = xPos
+		self.yPos = yPos
+		self.width = width
+		self.height = height
+		self.rectangle = (xPos, yPos, width, height)
+		self.color = color
+		self.surface = surface
+	def draw(self):
+		pg.draw.rect(self.surface, self.color, self.rectangle)
+
+	#Called upon Clicks
+	def wasClicked(self, clickPos):
+		if clickPos[0] > self.xPos and clickPos[0] < self.xPos + self.width:
+			if clickPos[1] > self.yPos and clickPos[1] < self.yPos + self.height:
+				return True
+		return False
+	
+class GameTile:
+	#Constants
+	MISS = 0
+	HIT = 1
+	BLANK = -1
+
+	#Members
+	def __init__(self, xPos, yPos, width, height, playerNumber, surface):
+		self.xPos = xPos
+		self.yPos = yPos
+		self.width = width
+		self.height = height
+		self.status = self.BLANK
+		self.surface = surface
+		self.hasBeenClicked = False
+		self.color = back_color
+		self.isClickableOverride = False
+		self.rectangle = (xPos, yPos, width, height)
+		self.ship = None
+		self.playerNumber = playerNumber
+		#Sound Effects
+		self.explosion_sound = pg.mixer.Sound("sounds\Explosion.wav")
+		self.splash_sound = pg.mixer.Sound("sounds\Splash.wav")
+
+	def wasClicked(self, clickPos):
+		if self.isClickable():
+			if clickPos[0] > self.xPos and clickPos[0] < self.xPos + self.width:
+				if clickPos[1] > self.yPos and clickPos[1] < self.yPos + self.height:
+					self.hasBeenClicked = True
+					if self.ship != None:
+						self.setStatus(self.HIT)
+						#Hit Sound
+						pg.mixer.Sound.play(self.explosion_sound)
+						self.ship.hit()
+						self.blowup_animation(clickPos)
+						self.shipDestroyedAnimation()
+					else:
+						self.setStatus(self.MISS)
+						self.splash_animation(clickPos)
+						#Miss Sound
+						pg.mixer.Sound.play(self.splash_sound)
+					return True
+		return False
+
+	def isClickable(self):
+		if self.isClickableOverride:
+			return False
+		return not self.hasBeenClicked
+
+	def setStatus(self, status):
+		self.status = status
+
+	def setClickable(self, setting):
+		self.isClickableOverride = setting
+
+	def blowup_animation(self,clickPos):
+		for image in Explosion_Images:
+			image = pg.transform.scale(image, (20,20))
+			self.surface.blit(image, clickPos)
+			pg.display.flip()
+			clock.tick(ExplosionSpeed)
+
+	def splash_animation(self,clickPos):
+		for image in Splash_Images:
+			image = pg.transform.scale(image, (20,20))
+			self.surface.blit(image, clickPos)
+			pg.display.flip()
+			clock.tick(SplashSpeed)
+
+	def draw(self, playerTurn):
+		self.color = back_color
+		if self.ship != None and (playerTurn == True or self.status == self.HIT or self.status == self.MISS):
+			if self.playerNumber == 2:
+				value = self.getShip().getLength()
+				self.color = (100 + value * 30, 100 + value * 30, 100 + value * 30 )
+				if back_color == WHITE:
+					self.color = (0 + value * 30, 0 + value * 30, 0 + value * 30)
+			if self.playerNumber == 1:
+				value = self.getShip().getLength()
+				self.color = (100 + value * 30, 100 + value * 30, 100 + value * 30 )
+				if back_color == WHITE:
+					self.color = (0 + value * 30, 0 + value * 30, 0 + value * 30)
+		elif self.playerNumber == 2:
+			self.color = back_color
+				
+		pg.draw.rect(self.surface, self.color, self.rectangle)
+
+		if not self.isClickable():
+			if self.status == self.HIT:
+				pg.draw.line(self.surface, text_color, (self.xPos, self.yPos), (24 + self.xPos, 24 + self.yPos))
+				pg.draw.line(self.surface, text_color, (24 + self.xPos, self.yPos), (self.xPos, 24 + self.yPos))
+			elif self.status == self.MISS:
+				pg.draw.circle(self.surface, text_color, (12 + self.xPos,12 + self.yPos), 12, 1)
+
+	def setShip(self, ship):
+		self.ship = ship
+
+	def shipDestroyedAnimation(self):
+		if self.ship.checkDestroyed():
+			font = pg.font.SysFont("none", 24)
+			if self.playerNumber == 1:
+				text = font.render("Player 2 ship destroyed!", True, text_color)
+				taunts = ["'You're a natural'", "'Easiest kill of my life!'", "'Roger, looks like we got a code E-Z.'", 
+					"'Are you even trying?'", "'Oops, did I do that?'", "'Was that ship made of paper?'"]
+				text2 = font.render(taunts[random.randint(0, 5)], True, text_color)
+			if self. playerNumber == 2:
+				text = font.render("Player 1 ship destroyed!", True, text_color)
+				taunts = ["'That was easy.'", "'Supreme Leader South will love that'", "'RIP Jack Sparrow'", "'FRESH MEAT!'", 
+					"'This predicament will be over in no time'", "'We live in a society'" ]
+				text2 = font.render(taunts[random.randint(0, 5)], True, text_color)
+			loopFinished = False
+			while loopFinished == False:
+				for event in pg.event.get():
+					if event.type == pg.QUIT:
+						pg.quit()
+						sys.exit()
+					if event.type == pg.MOUSEBUTTONDOWN:
+							loopFinished = True
+				self.surface.fill(BLACK)
+				self.surface.blit(text, (w /2 - text.get_width() / 2, 50))
+				self.surface.blit(text2, (w /2 - text.get_width() / 2, h / 2))
+				pg.display.update()
+
+	def getShip(self):
+		return self.ship
+		
+class Player:
+	#Constants
+	LEFT = 0
+	UP = 1
+	RIGHT = 2
+	DOWN = 3
+	
+	def __init__(self, setting, playerNumber, surface, grid_size):
+		self.myTurn = setting
+		self.playerNumber = playerNumber
+		self.surface = surface
+		self.buttonTiles = []
+		self.ships = [Ship(5), Ship(4), Ship(3), Ship(3), Ship(2)]
+		for i in range(grid_size):
+			temp = []
+			for j in range(grid_size):
+				if self.playerNumber == 1:
+					temp.append(GameTile(int((w/2) - (24*5 + 24*grid_size)) + 24*i, int(h/3) + 24*j, 24, 24, 1, self.surface))
+				else:
+					temp.append(GameTile(int((w/2) + (24*5 + 24*grid_size)) - 24*i - 24, int(h/3) + 24*j, 24, 24, 1, self.surface))
+
+			self.buttonTiles.append(temp)
+		self.distributeShips(grid_size)
+			
+	def drawTiles(self):
+		for row in self.buttonTiles:
+			for tile in row:
+				tile.draw(self.myTurn)
+				
+	def flipTurn(self):
+		self.myTurn = not self.myTurn
+
+	def getButtonTiles(self):
+		return self.buttonTiles
+
+	def distributeShips(self, grid_size):
+		for ship in self.ships:
+			done = False
+			while not done:
+				row = random.randint(0, grid_size-1)
+				column = random.randint(0, grid_size-1)
+				direction = random.randint(self.LEFT, self.DOWN)
+				if self.checkIfSectionEmpty(row, column, direction, ship):
+					self.placeShip(row, column, direction, ship)
+					done = True
+
+	def getTurn(self):
+		return self.myTurn
+
+	def checkIfSectionEmpty(self, row, column, direction, ship):
+		if self.buttonTiles[row][column].getShip() == None:
+			if direction == self.LEFT:
+				if column - ship.getLength() < 0:
+					return False
+				else:
+					for i in range(column, column - ship.getLength(), -1):
+						if self.buttonTiles[row][i].getShip() != None:
+							return False
+					return True
+				
+			elif direction == self.UP:
+				if row - ship.getLength() < 0:
+					return False
+				else:
+					for i in range(row, row - ship.getLength(), -1):
+						if self.buttonTiles[i][column].getShip() != None:
+							return False
+					return True
+				
+			elif direction == self.RIGHT:
+				if column + ship.getLength() > 5:
+					return False
+				else:
+					for i in range(column, column + ship.getLength()):
+						if self.buttonTiles[row][i].getShip() != None:
+							return False
+					return True
+				
+			elif direction == self.DOWN:
+				if row + ship.getLength() > 5:
+					return False
+				else:
+					for i in range(row, row + ship.getLength()):
+						if self.buttonTiles[i][column].getShip() != None:
+							return False
+					return True
+		else:
+			return False
+
+	def placeShip(self, row, column, direction, ship):
+		if direction == self.LEFT:
+			for i in range(column, column - ship.getLength(), -1):
+				self.buttonTiles[row][i].setShip(ship)
+				
+		if direction == self.UP:
+			for i in range(row, row - ship.getLength(), -1):
+				self.buttonTiles[i][column].setShip(ship)
+				
+		if direction == self.RIGHT:
+			for i in range(column, column + ship.getLength(), 1):
+				self.buttonTiles[row][i].setShip(ship)
+				
+		if direction == self.DOWN:
+			for i in range(row, row + ship.getLength(), 1):
+				self.buttonTiles[i][column].setShip(ship)
+	def getPlayerNumber(self):
+		return self.playerNumber
+
+	def allShipsDestroyed(self):
+		for ship in self.ships:
+			for value in ship.getHitList():
+				if value == False:
+					return False
+		return True
+				
+class Ship:
+	def __init__(self, length):
+		self.length = length
+		self.hitList = [False] * self.length
+
+	def hit(self):
+		shipRemoved = False
+		iterator = 0
+		while shipRemoved == False:
+			if self.hitList[iterator] == False:
+				self.hitList[iterator] = True
+				shipRemoved = True
+			iterator += 1
+
+	def getLength(self):
+		return self.length
+
+	def checkDestroyed(self):
+		for i in self.hitList:
+			if i == False:
+				return False
+		return True
+
+	def getHitList(self):
+		return self.hitList
+
+#--------------------------
+# Grid creation
+#--------------------------
+def drawGrid(surface,grid_size):
+
+	center_offset_left = int((w/2) - (24*5 + 24*grid_size))
+	center_offset_right = int((w/2) + 24*5)
+
+	#First grid
+	#Vertical Lines
+	for i in range(0, 24*grid_size+1, 24):
+		pg.draw.line(surface, text_color, (i + center_offset_left, h/3), (i + center_offset_left, (h/3)+24*grid_size))
+	# Horizontal Lines
+	for i in range(int(h/3), int(h/3)+24*grid_size+1, 24):
+		pg.draw.line(surface, text_color, (0 + center_offset_left, i), (center_offset_left + 24*grid_size, i))
+
+	#Second grid
+	#Vertical Lines
+	for i in range(center_offset_right, center_offset_right + 24*grid_size + 24, 24):
+		pg.draw.line(surface, text_color, (i, (h/3)), (i, (h/3)+24*grid_size))
+	# Horizontal Lines
+	for i in range(int(h/3), int(h/3)+24*grid_size+1, 24):
+		pg.draw.line(surface, text_color, (center_offset_right, i), (center_offset_right + 24*grid_size, i))
+
+#--------------------------
+# Music For In Game Play
+#--------------------------
+def gameplay_music_engine(volume_level):
+	pg.init()
+	pg.mixer.music.stop()
+	pg.mixer.music.load('sounds\MusicGunnerFight.ogg')
+	pg.mixer.music.set_volume(volume_level)
+	pg.mixer.music.play(-1)
+
+#--------------------------
+# Gameplay Controller
+#--------------------------
+def RunGame(grid_size, volume_level, color_text, color_background):
+	global text_color
+	global back_color
+
+	pg.init()
+
+	text_color = color_text
+	back_color = color_background
+
+	player1 = Player(True, 1, surface, grid_size)
+	player2 = Player(False, 2, surface, grid_size)
+
+	upNext = 2
+	limboMode = False 
+
+	font = pg.font.SysFont("none", 24)
+	text2 = font.render("Click to begin turn", True, text_color)
+	exitButton = Button(w /2 -62.5, h - h/4 - 25, 125, 50, text_color, surface)
+
+	#Sounds begin playing after initialization of drawings
+	gameplay_music_engine(volume_level)
+	game_over_sound = pg.mixer.Sound("sounds\SmallApplause.wav")
+
+	while True:
+		#Checking for game events'
+		tileClicked = False
+		for event in pg.event.get():
+			if event.type == pg.QUIT:
+				pg.quit()
+				sys.exit()
+			elif event.type == pg.KEYDOWN:
+				if event.key == pg.K_ESCAPE:
+					done = True
+					pg.mixer.music.stop()
+					#Return 0 for main menu
+					return 0
+				
+			if event.type == pg.MOUSEBUTTONDOWN:
+				if player2.getTurn() == True:
+					for row in player1.getButtonTiles():
+						for tile in row:
+							feels = tile.wasClicked(pg.mouse.get_pos())
+							if feels == True:
+								tileClicked = True
+					if exitButton.wasClicked(pg.mouse.get_pos()):
+						#Return 0 for main menu
+						return 0
+
+				if player1.getTurn() == True:		
+					for row in player2.getButtonTiles():
+						for tile in row:
+							feels = tile.wasClicked(pg.mouse.get_pos())
+							if feels == True:
+								tileClicked = True
+					if exitButton.wasClicked(pg.mouse.get_pos()):
+						#Return 0 for main menu
+						return 0
+								
+				if limboMode == True:
+					if upNext == 2:
+						player2.flipTurn()
+						upNext = 1
+						limboMode = False
+					else:
+						player1.flipTurn()
+						upNext = 2
+						limboMode = False
+			
+							
+			if tileClicked == True:
+				if player1.getTurn() == True:
+					player1.flipTurn()
+					limboMode = True
+				elif player2.getTurn() == True:
+					player2.flipTurn()
+					limboMode = True
+
+		surface.fill(back_color)
+		player1.drawTiles()
+		player2.drawTiles()
+		exit_text = font.render("Exit to Menu", True, back_color)
+		exitButton.draw()
+		surface.blit(exit_text, (w/2 - exit_text.get_width() / 2, h - h/4 - 5))
+		if not limboMode:
+			if upNext == 1:
+				text = font.render("Player 2's turn!", True, text_color)
+			else:
+				text = font.render("Player 1's turn!", True, text_color)
+			surface.blit(text,(w /2 - text.get_width() / 2, 50))
+		else:
+			text = font.render("Player " + str(upNext) + " is up next! Click to continue", True, text_color)
+			surface.blit(text,(w /2 - text.get_width() / 2, 50))
+		drawGrid(surface, grid_size)
+		pg.display.update()
+
+		if player1.allShipsDestroyed():
+			pg.mixer.Sound.play(game_over_sound)
+			return 2
+			
+		if player2.allShipsDestroyed():
+			pg.mixer.Sound.play(game_over_sound)
+			return 1
---
+++ Menus.py
@@ -0,0 +1,405 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import sys
+import pygame as pg
+import pygame_menu as pm
+from pygame_menu import sound
+import Game
+from Game import RunGame
+
+#---------------------
+#Constants and Globals
+#---------------------
+
+#Set surface as Fullscreen Display
+surface = pg.display.set_mode((0,0),pg.FULLSCREEN)
+
+#Used for debugging
+#Note: Do not actually resize the window, this may cause window errors
+#surface = pg.display.set_mode((700,700),pg.RESIZABLE)
+
+#Color List for quick reference
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+LIME = (0,255,0)
+BLUE = (0,0,255)
+YELLOW = (255,255,0)
+CYAN = (0,255,255)
+MAGENTA = (255,0,255)
+SILVER = (192,192,192)
+GRAY = (128,128,128)
+MAROON = (128,0,0)
+OLIVE = (128,128,0)
+GREEN = (0,128,0)
+PURPLE = (128,0,128)
+TEAL = (0,128,128)
+NAVY = (0,0,128)
+
+#Color set for Text
+color = RED
+color_default = 0
+
+#Color set for Background
+back_color = BLACK
+back_color_default = 0
+
+#Volume set for sounds
+volume_level = 0.2
+volume_default = 2
+
+#Player set for play menu
+players = 1
+player_default = 0
+player_str = 'Players: 1 :'
+
+#Difficulty set for play menu
+difficulty = 1
+difficulty_default = 0
+difficulty_str = 'Difficulty: Easy :'
+
+#Grid size set for play menu
+grid_size = 10
+grid_default = 1
+grid_str = 'Grid Size: 10 x 10 :'
+
+#---------------------
+#Methods/Functions
+#---------------------
+
+#Method: Sets color for text based on input
+def set_color(name, value):
+	global color
+	global color_default
+
+	if value == 1:
+		color = RED #RED
+		color_default = value-1
+		options_menu()
+	if value == 2:
+		color = BLUE #BLUE
+		color_default = value-1
+		options_menu()
+	if value == 3:
+		color = GREEN #GREEN
+		color_default = value-1
+		options_menu()
+	else:
+		color = RED #RED BACKUP
+		color_default = 0
+		options_menu()
+
+#Method: sets color of background based on input
+def set_background(name, value):
+	global back_color
+	global back_color_default
+
+	if value == 1:
+		back_color = BLACK #BLACK
+		back_color_default = value-1
+		options_menu()
+	if value == 2:
+		back_color = WHITE #WHITE
+		back_color_default = value-1
+		options_menu()
+	if value == 3:
+		back_color = GRAY #GRAY
+		back_color_default = value-1
+		options_menu()
+	else:
+		back_color = BLACK #BLACK BACKUP
+		back_color_default = 0
+		options_menu()
+
+#Method: Creates the theme used for all menus
+def set_theme():
+	pg.init()
+	screen_res = pg.display.Info()
+	w = screen_res.current_w
+	new_w_offset = w/2 - len("The Ship Predicament")/0.0625
+
+	eight_bit_font = pm.font.FONT_8BIT
+	title_theme = pm.widgets.MENUBAR_STYLE_NONE
+	font_size = 37
+	select_box_color = WHITE
+	if back_color == WHITE:
+		select_box_color = BLACK
+		
+	current_theme = pm.themes.Theme(background_color=back_color, widget_font=eight_bit_font, title_bar_style=title_theme, 
+							menubar_close_button=True, title_font=eight_bit_font, title_font_color=color, 
+							title_font_size=font_size, title_offset=(new_w_offset,0), selection_color=select_box_color)
+	return current_theme
+
+#Method: Creates the sound engine used with menus and gameplay
+def create_sound_engine():
+	engine = sound.Sound()
+	engine.set_sound(sound.SOUND_TYPE_CLICK_MOUSE, sound.SOUND_EXAMPLE_CLICK_MOUSE)
+	engine.set_sound(sound.SOUND_TYPE_CLOSE_MENU, sound.SOUND_EXAMPLE_CLOSE_MENU)
+	engine.set_sound(sound.SOUND_TYPE_ERROR, sound.SOUND_EXAMPLE_ERROR)
+	engine.set_sound(sound.SOUND_TYPE_EVENT, sound.SOUND_EXAMPLE_EVENT)
+	engine.set_sound(sound.SOUND_TYPE_EVENT_ERROR, sound.SOUND_EXAMPLE_EVENT_ERROR)
+	engine.set_sound(sound.SOUND_TYPE_KEY_ADDITION, sound.SOUND_EXAMPLE_KEY_ADD)
+	engine.set_sound(sound.SOUND_TYPE_KEY_DELETION, sound.SOUND_EXAMPLE_KEY_DELETE)
+	engine.set_sound(sound.SOUND_TYPE_OPEN_MENU, sound.SOUND_EXAMPLE_OPEN_MENU)
+	engine.set_sound(sound.SOUND_TYPE_WIDGET_SELECTION, sound.SOUND_EXAMPLE_WIDGET_SELECTION)
+	return engine
+
+#Method: Begins background music play, stops upon exiting the game
+def background_music_loop():
+	global volume_level
+	pg.init()
+	pg.mixer.music.load('sounds\MusicGameplay.ogg')
+	pg.mixer.music.set_volume(volume_level)
+	pg.mixer.music.play(-1) #loops forevever while game is up
+
+#Method: Sets the volume level and default for the sound engine
+#Note: volume levels are only set between 0.0 and 1.0
+def set_volume_level(name, value):
+	global volume_level
+	global volume_default
+	volume_level = value * 0.1
+	volume_default = value
+	pg.init()
+#	pg.mixer.music.stop()
+	background_music_loop()
+	options_menu()
+
+#Method: Sets the items to be used by the options menu volume selector
+def set_volume_items():
+	volume_int_list = []
+	volume_str_list = []
+	volume_int_list = [i for i in range(0,11)]
+	for a in volume_int_list:
+		a = str(a)
+		volume_str_list.append(a)
+	volume_items = list(zip(volume_str_list, volume_int_list))
+	return volume_items
+
+#Method: Sets the difficulty level for Single Player games
+def set_difficulty(name, value):
+	global difficulty
+	global difficulty_default
+	global difficulty_str
+	
+	if value == 1:
+		difficulty = value #EASY
+		difficulty_default = value - 1
+		difficulty_str = 'Difficulty: Easy :'
+		play_menu()
+	if value == 2:
+		difficulty = value #MEDIUM
+		difficulty_default = value - 1
+		difficulty_str = 'Difficulty: Medium :'
+		play_menu()
+	if value == 3:
+		difficulty = value #HARD
+		difficulty_default = value - 1
+		difficulty_str = 'Difficulty: Hard :'
+		play_menu()
+	else:
+		difficulty = 1 #EASY DEFAULT
+		difficulty_default = 0
+		difficulty_str = 'Difficulty: Easy :'
+		play_menu()
+
+#Method: Sets the number of players, 1 or 2
+def set_players(name, value):
+	global players
+	global player_default
+	global player_str
+
+	if value == 1:
+		players = value #ONE PLAYER
+		player_default = value - 1
+		player_str = 'Players: 1 :'
+		play_menu()
+	if value == 2:
+		players = value #TWO PLAYERS
+		player_default = value - 1
+		player_str = 'Players: 2 :'
+		play_menu()
+	else:
+		players = 1 #ONE PLAYER DEFAULT
+		difficulty_default = 0
+		player_str = 'Players: 1 :'
+		play_menu()
+
+#Method: Sets the grid size to use during play
+def set_grid_size(name, value):
+	global grid_size
+	global grid_default
+	global grid_str
+
+	if value == 7:
+		grid_size = value # 7x7
+		grid_default = 0
+		grid_str = 'Grid Size: 7 x 7 :'
+		play_menu()
+	if value == 10:
+		grid_size = value # 10x10
+		grid_default = 1
+		grid_str = 'Grid Size: 10 x 10 :'
+		play_menu()
+	if value == 15:
+		grid_size = value # 15x15
+		grid_default = 2
+		grid_str = 'Grid Size: 15 x 15 :'
+		play_menu()
+	else:
+		grid_size = 10 # 10x10 DEFAULT
+		grid_default = 1
+		grid_str = 'Grid Size: 10 x 10 :'
+		play_menu()
+
+#--------------------------
+# Actual Game
+#--------------------------
+def start_the_game():
+	global volume_level
+	global color
+	global back_color
+	
+	who_won = RunGame(grid_size, volume_level, color, back_color)
+	background_music_loop()
+
+	if who_won == 0:
+		main_menu()
+	else:
+		game_over_menu(who_won)
+	
+#--------------------------
+# Main Menu
+#--------------------------
+def main_menu():
+	pg.init()
+	global surface
+	global color
+	global back_color
+	screen_res = pg.display.Info()
+	h = screen_res.current_h
+	w = screen_res.current_w
+
+	mytheme = set_theme()
+	sound_engine = create_sound_engine()
+
+	menu = pm.Menu(h, w, 'The Ship Predicament', theme=mytheme)
+
+	menu.add_button('Play', play_menu, font_color=color)
+	menu.add_button('Options', options_menu, font_color=color)
+	menu.add_button('Exit', pm.events.EXIT, font_color=color)
+	menu.set_sound(sound_engine)
+
+	menu.mainloop(surface)
+
+#--------------------------
+# Options Menu
+#--------------------------
+def options_menu():
+	pg.init()
+	global surface
+	global color
+	global back_color
+	global volume_default
+
+	screen_res = pg.display.Info()
+	h = screen_res.current_h
+	w = screen_res.current_w
+
+	mytheme = set_theme()
+	sound_engine = create_sound_engine()
+	volume_items = set_volume_items()
+	
+	options_sub = pm.Menu(h, w, 'Options', theme=mytheme)
+
+	options_sub.add_label('Press Enter To')
+	options_sub.add_label('Apply Selected Item')
+	options_sub.add_vertical_margin(30)
+	options_sub.add_selector('Music Volume ', volume_items, default=volume_default, onreturn=set_volume_level, font_color=color)
+	options_sub.add_selector('Text Color ', [('Red', 1), ('Blue', 2), ('Green', 3)], default=color_default, onreturn=set_color, font_color=color)
+	options_sub.add_selector('Background ', [('Black', 1), ('White', 2), ('Gray', 3)], default=back_color_default, onreturn=set_background, font_color=color)
+	options_sub.add_vertical_margin(50)
+	options_sub.add_button('[ Main Menu ]', main_menu, font_color=color)
+	options_sub.set_sound(sound_engine)
+	
+	options_sub.mainloop(surface)
+
+#-------------------------------------------
+# Play Menu and Final Settings Confirmation
+#-------------------------------------------
+def play_menu():
+	pg.init()
+	global surface
+	global color
+	global back_color
+
+	screen_res = pg.display.Info()
+	h = screen_res.current_h
+	w = screen_res.current_w
+
+	mytheme = set_theme()
+	sound_engine = create_sound_engine()
+
+	play_setting_sub = pm.Menu(h, w, 'Confirm Settings', theme=mytheme)
+
+	play_setting_sub.add_label(player_str)
+	play_setting_sub.add_label(difficulty_str)
+	play_setting_sub.add_label(grid_str)
+	play_setting_sub.add_vertical_margin(30)
+	play_setting_sub.add_button('[ Confirm ]', start_the_game, font_color=color)
+	play_setting_sub.add_button('[ Back ]', pm.events.BACK, font_color=color)
+	play_setting_sub.set_sound(sound_engine)
+
+	play_sub = pm.Menu(h, w, 'Game Setup', theme=mytheme)
+
+	play_sub.add_label('Press Enter To')
+	play_sub.add_label('Apply Selected Item')
+	play_sub.add_vertical_margin(30)
+	play_sub.add_selector('Players ', [('1 Player', 1), ('2 Player', 2)], default=player_default, onreturn=set_players, font_color=color)
+	play_sub.add_selector('Difficulty ', [('Easy', 1), ('Medium', 2), ('Hard', 3)], default=difficulty_default, onreturn=set_difficulty, font_color=color)
+	play_sub.add_selector('Grid Size ', [('7 x 7', 7), ('10 x 10', 10), ('15 x 15', 15)], default=grid_default, onreturn=set_grid_size, font_color=color)
+	play_sub.add_button('[ Go ]', play_setting_sub, font_color=color)
+	play_sub.add_vertical_margin(50)
+	play_sub.add_button('[ Main Menu ]', main_menu, font_color=color)
+	play_sub.set_sound(sound_engine)
+
+	play_sub.mainloop(surface)
+
+#-------------------------------------------
+# Display Screen for Game Completion
+#-------------------------------------------
+def game_over_menu(who_won):
+	pg.init()
+	global surface
+	global color
+	global back_color
+	global volume_default
+
+	screen_res = pg.display.Info()
+	h = screen_res.current_h
+	w = screen_res.current_w
+
+	#If 1 Player 1 won, If 2 Player 2 won
+	player_won = who_won
+	Winner_Str = ''
+	if player_won == 1:
+		Winner_Str = 'Player 1 Wins'
+	elif player_won == 2:
+		Winner_Str = 'Player 2 Wins'
+
+	mytheme = set_theme()
+	sound_engine = create_sound_engine()
+	
+	game_over_sub = pm.Menu(h, w, 'Game Over', theme=mytheme)
+
+	game_over_sub.add_label(Winner_Str)
+	game_over_sub.add_image('img\GameOverBattleship.jpg')
+	game_over_sub.add_vertical_margin(30)
+	game_over_sub.add_button('[ Play Again ]', play_menu, font_color=color)
+	game_over_sub.add_button('[ Main Menu ]', main_menu, font_color=color)
+	game_over_sub.add_button('[ Exit ]', pm.events.EXIT, font_color=color)
+	game_over_sub.set_sound(sound_engine)
+	
+	game_over_sub.mainloop(surface)
+
+#Had to add the main_menu() call on my end...working on a Mac.
+#main_menu()
